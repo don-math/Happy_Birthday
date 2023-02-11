9abad9dfbe5ca603f39489c64f025c6272ce5505
Dongdong Lu <LDDCZCN@GMAIL.COM>
2023-02-11 12:46:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.android.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.happybirthday.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container that uses the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    BirthdayGreetingWithImage( "Happy Birthday Sam!", "- from Emma")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+        )
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String){
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(painter = image, contentDescription = null)
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        BirthdayGreetingWithImage( "Happy Birthday Sam!", "- from Emma")
+    }
+}
